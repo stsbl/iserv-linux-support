3fd9cbc73837866257502a97aac2d5117deb197e
Felix Jacobi <felix.jacobi@stsbl.de>
2017-04-18 21:04:11 +0200

    Added auto_yes and verbose to SCP options.

--- usr/share/perl5/Stsbl/IServ/SCP.pm
+++ usr/share/perl5/Stsbl/IServ/SCP.pm
@@ -15,7 +15,7 @@ BEGIN
 sub scp($$$)
 {
   my ($host, $source, $destination) = @_;
-  my $scp = Net::SCP::Expect->new(identity_file => "/var/lib/iserv/config/id_rsa");
+  my $scp = Net::SCP::Expect->new(identity_file => "/var/lib/iserv/config/id_rsa", verbose => 1, auto_yes => 1);
   $scp->host($host);
   $scp->login("root");
   $scp->scp($source, $destination);
